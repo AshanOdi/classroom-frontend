b9ff1a9aff39b8f711d8944291490e5427effb02
AshanOdi <ashanuni25@gmail.com>
2026-05-24 20:50:18 +0530

    feat: try to create data table

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const DEPARTMENT = ["CS", "IT", "ME", "CIVIL", "MCA", "BCA"];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENT.map((dept) => ({
+  label: dept,
+  value: dept,
+}));
--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,7 +1,107 @@
-import React from "react";
+import { CreateButton } from "@/components/refine-ui/buttons/create";
+import { DataTable } from "@/components/refine-ui/data-table/data-table";
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { DEPARTMENT, DEPARTMENT_OPTIONS } from "@/constants";
+import { Subject } from "@/types";
+import { useTable } from "@refinedev/core";
+import { Badge, Filter, Search } from "lucide-react";
+import React, { useMemo, useState } from "react";
 
 const SubjectList = () => {
-  return <div>SubjectList</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedDepartment, setSelectedDepartment] = useState("all");
+
+  const subjectTable = useTable<Subject>({
+    resource: "subjects",
+    pagination: {
+      pageSize: 10,
+      mode: "server",
+    },
+    filters: {},
+    sorters: {},
+  });
+
+  const columns = useMemo(
+    () => [
+      {
+        id: "code",
+        accessorKey: "code",
+        size: 100,
+        header: () => <p className="column-title ml-2">Code</p>,
+        cell: ({ getValue }) => <Badge>{getValue<string>()}</Badge>,
+      },
+    ],
+    [],
+  );
+  console.log(subjectTable);
+
+  return (
+    <ListView>
+      <Breadcrumb />
+      <h1 className="page-title">Subjects</h1>
+      <div className="intro-row">
+        <p>Quick access to essential metrics and management tools.</p>
+        <div className="actions-row">
+          <div className="search-field">
+            <Search className="search-icon" />
+            <input
+              type="text"
+              placeholder="Search by Name..."
+              className="h-9
+    w-full
+    rounded-md
+    border
+    border-border
+    bg-background
+    px-10
+    text-sm
+    text-foreground
+    placeholder:text-muted-foreground
+    transition-colors
+    focus:outline-none
+    focus:ring-1
+    focus:ring-primary
+    focus:border-primary"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+
+          <div className="flex gap-2 w-full md:w-auto">
+            <Select
+              value={selectedDepartment}
+              onValueChange={setSelectedDepartment}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Filter by Department" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectItem value="all">All Departments</SelectItem>
+
+                {DEPARTMENT_OPTIONS.map((dept) => (
+                  <SelectItem key={dept.value} value={dept.value}>
+                    {dept.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <CreateButton />
+          </div>
+        </div>
+      </div>
+
+      <DataTable column={columns} table={subjectTable} />
+    </ListView>
+  );
 };
 
 export default SubjectList;
